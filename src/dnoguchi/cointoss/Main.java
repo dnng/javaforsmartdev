a627d140d1f9faa601162bebbe05203a0ebb35e2
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-15 14:22:58 -0800

    Add initial main class

---
+++ src/dnoguchi/cointoss/Main.java
@@ -0,0 +1,23 @@
+package dnoguchi.cointoss;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Coin coin = new Coin();
+        int headcount = 0;
+
+        System.out.println("Initial coin toss value is: " + coin.getSideUp());
+
+        for (int i = 0; i < 20; i++) {
+            coin.toss();
+            if (coin.getSideUp().equals("heads")) {
+                headcount++;
+            }
+
+            System.out.println("New toss: " + coin.getSideUp());
+        }
+
+        System.out.println("Total heads count: " + headcount);
+        System.out.println("Total tails count: " + (20 - headcount));
+    }
+}
