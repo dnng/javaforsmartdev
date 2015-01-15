0287cf912f732cc4ca663c4eafac25c2818916ef
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-15 15:47:47 -0800

    Add initial parked car class

--- src/dnoguchi/parkingticket/ParkedCar.java
+++ src/dnoguchi/parkingticket/ParkedCar.java
@@ -1,5 +1,7 @@
 package dnoguchi.parkingticket;
 
+import java.util.Date;
+
 /**
  * Created by dnoguchi on 1/15/15.
  */
@@ -8,8 +10,62 @@ public class ParkedCar {
     private String model;
     private String color;
     private String licenseNumber;
-    private int timeParked;
+    private long timeParked;
+    private Date dateCreated;
+    private ParkingMeter purchasedTime;
 
     public ParkedCar() {
+        dateCreated = new Date(); // sets to current date/time
+        purchasedTime = new ParkingMeter();
     }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public long getTimeParked() {
+        timeParked = dateCreated.getTime() / 60000; // milliseconds to minutes
+        return timeParked;
+    }
+
+    public void setTimeParked(int timeParked) {
+        this.timeParked = timeParked;
+    }
+
+    public void purchaseTime(int timeToBePurchased) {
+        purchasedTime.setPurchasedTime(timeToBePurchased);
+    }
+
+    public void getPurchasedTime() {
+        purchasedTime.getPurchasedTime();
+    }
+
 }
