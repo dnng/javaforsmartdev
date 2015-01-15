050a3d5ab0cb0c4ae8ae35748b275e6a9b89517d
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-15 15:50:14 -0800

    Add initial police officer class

---
+++ src/dnoguchi/parkingticket/PoliceOfficer.java
@@ -0,0 +1,33 @@
+package dnoguchi.parkingticket;
+
+public class PoliceOfficer {
+    private String name;
+    private int badgeNumber;
+
+    public PoliceOfficer() {
+    }
+
+    public void examineParking(ParkedCar car) {
+
+    }
+
+    public void issueTicket() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBadgeNumber() {
+        return badgeNumber;
+    }
+
+    public void setBadgeNumber(int badgeNumber) {
+        this.badgeNumber = badgeNumber;
+    }
+}
