5e0dd31593e5e28ed9611823531a1035cc1708c4
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-15 15:48:05 -0800

    Add initial parking meter class

--- src/dnoguchi/parkingticket/ParkingMeter.java
+++ src/dnoguchi/parkingticket/ParkingMeter.java
@@ -1,7 +1,21 @@
 package dnoguchi.parkingticket;
 
+import java.sql.Time;
+
 /**
  * Created by dnoguchi on 1/15/15.
  */
 public class ParkingMeter {
+    private int purchasedTime;
+
+    public ParkingMeter() {
+    }
+
+    public void setPurchasedTime(int purchasedTime) {
+        this.purchasedTime = purchasedTime;
+    }
+
+    public int getPurchasedTime() {
+        return purchasedTime;
+    }
 }
