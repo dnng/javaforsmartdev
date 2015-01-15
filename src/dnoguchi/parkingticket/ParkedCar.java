a4bfa1a72f7f662198e155c3bf0bfb2adf594d33
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-15 14:42:29 -0800

    Add initial ParkedCar class

---
+++ src/dnoguchi/parkingticket/ParkedCar.java
@@ -0,0 +1,15 @@
+package dnoguchi.parkingticket;
+
+/**
+ * Created by dnoguchi on 1/15/15.
+ */
+public class ParkedCar {
+    private String make;
+    private String model;
+    private String color;
+    private String licenseNumber;
+    private int timeParked;
+
+    public ParkedCar() {
+    }
+}
