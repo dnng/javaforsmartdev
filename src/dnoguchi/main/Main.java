c34e56058a138631eec1d93bfe2001be1b180f8c
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-17 12:37:17 -0800

    Update main method and add parking code

--- src/dnoguchi/main/Main.java
+++ src/dnoguchi/main/Main.java
@@ -1,30 +1,43 @@
 package dnoguchi.main;
 
 import dnoguchi.cointoss.Coin;
+import dnoguchi.parkingticket.ParkedCar;
 import dnoguchi.parkingticket.ParkingMeter;
+import dnoguchi.parkingticket.PoliceOfficer;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+
+    public static void main(String[] args) {
+        List<String> programList = Arrays.asList("coin", "parking");
+
+        /*
+         * Simulate for..else python loop
+         */
+        select: {
+            for(String program: programList) {
+                if (args[0].equals(program)) {
+                    call(program);
+                    break select;
+                }
+            }
+            System.out.println("Please specify a valid program name");
+        }
+
+        /*
+         * Force parking call for testing purposes
+         */
+        Parking();
+    }
 
     public static void call(String program) {
         if (program.equals("coin")) {
             Coin();
         } else if (program.equals("parking")) {
             Parking();
-        } else {
-            System.out.println("Please specify a valid program name");
-        }
-    }
-
-    public static void main(String[] args) {
-        List<String> programList = Arrays.asList("coin", "parking");
-
-        for(String program: programList) {
-            if (args[0].equals(program)) {
-                call(program);
-            }
         }
     }
 
@@ -53,6 +66,22 @@ public class Main {
          * 1) Create two parked cars
          * 2) Make the Police officer check both of them and issue one ticket
          */
+        ParkedCar car1 = new ParkedCar("Chevrolet", "Cobra", "blue", "123456", 1);
+        ParkedCar car2 = new ParkedCar("Tesla", "S1", "black", "3141592", 200);
+
+        PoliceOfficer officer = new PoliceOfficer("Danny", 1337);
+
+        officer.examineParking(car1);
+        officer.examineParking(car2);
+
+        try {
+            TimeUnit.MINUTES.sleep(2);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        officer.examineParking(car1);
     }
 
 }
