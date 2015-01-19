d4f38eba78c17f72732860c08e81eac47bfb05e0
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-18 20:21:32 -0800

    Add tokenizer code

--- src/dnoguchi/studentrecords/Util.java
+++ src/dnoguchi/studentrecords/Util.java
@@ -3,33 +3,30 @@ package dnoguchi.studentrecords;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Util {
-    public static Student [] readFile(String filename, Student[] stu) {
-        //Reads the file and builds student array.
-        //Open the file using FileReader Object.
-        //In a loop read a line using readLine method.
-        //Tokenize each line using StringTokenizer Object
-        //Each token is converted from String to Integer using parseInt method
-        //Value is then saved in the right property of Student Object.
-        ArrayList<Student> students = new ArrayList<Student>();
-
+    public static ArrayList<Student> readFile(String filename, ArrayList<Student> stu) {
         try {
-            FileReader file = new FileReader("ReadSource.java");
+            FileReader file = new FileReader("studentrecords.txt");
             BufferedReader buff = new BufferedReader(file);
             boolean eof = false;
             while (!eof) {
                 String line = buff.readLine();
                 if (line == null)
                     eof = true;
-                else
+                else {
+                    StringTokenizer st = new StringTokenizer(line, " ");
+                    while (st.hasMoreTokens()) {
+                        System.out.println(st.nextToken());
+                    }
                     System.out.println(line);
+                }
             }
-
             buff.close();
-        } catch (IOException e) {System.out.println("Error -- " + e.toString());
+        } catch (IOException e) {
+            System.out.println("Error -- " + e.toString());
         }
 
     }
