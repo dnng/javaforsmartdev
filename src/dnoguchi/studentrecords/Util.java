bab792dfdadbf21d28300c796924637af5a3c536
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-18 16:21:42 -0800

    Update file read and student records

--- src/dnoguchi/studentrecords/Util.java
+++ src/dnoguchi/studentrecords/Util.java
@@ -3,15 +3,18 @@ package dnoguchi.studentrecords;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Util {
-    static Student [] readFile(String filename, Student [] stu) {
+    public static Student [] readFile(String filename, Student[] stu) {
         //Reads the file and builds student array.
         //Open the file using FileReader Object.
         //In a loop read a line using readLine method.
         //Tokenize each line using StringTokenizer Object
         //Each token is converted from String to Integer using parseInt method
         //Value is then saved in the right property of Student Object.
+        ArrayList<Student> students = new ArrayList<Student>();
 
         try {
             FileReader file = new FileReader("ReadSource.java");
