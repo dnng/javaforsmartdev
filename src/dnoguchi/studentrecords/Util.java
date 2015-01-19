80887061a7f90bcdae10c8dd98fcbb18fd0ec3a4
Daniel Noguchi <danielnoguchi@gmail.com>
2015-01-19 09:52:41 -0800

    Implement read student record method

--- src/dnoguchi/studentrecords/Util.java
+++ src/dnoguchi/studentrecords/Util.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Util {
@@ -12,17 +13,41 @@ public class Util {
             FileReader file = new FileReader("studentrecords.txt");
             BufferedReader buff = new BufferedReader(file);
             boolean eof = false;
+
+            /* quick and dirty way to skip the first line */
+            String line = buff.readLine();
             while (!eof) {
-                String line = buff.readLine();
+
+                /* dirty indexing of students array */
+                int i = 0;
+
+                line = buff.readLine();
                 if (line == null)
                     eof = true;
                 else {
+                    /*
+                     * Learning new types and Interfaces in Java, clean this mess after playing! :-P
+                     *
+                     * All this has to do is populate the Students Array with the records information.
+                     */
                     StringTokenizer st = new StringTokenizer(line, " ");
+                    String token = st.nextToken();
+                    stu.get(i).setSID(Integer.parseInt(token));
+
+                    ArrayList<Integer> scores = new ArrayList<Integer>();
+
                     while (st.hasMoreTokens()) {
-                        System.out.println(st.nextToken());
+                        scores.add(Integer.parseInt(st.nextToken()));
                     }
-                    System.out.println(line);
+
+                    int [] bucket = new int[scores.size()];
+                    Iterator<Integer> iterator = scores.iterator();
+                    for (int idx = 0; idx < bucket.length; idx++) {
+                        bucket[i] = iterator.next().intValue();
+                    }
+                    stu.get(i).setScores(bucket);
                 }
+                i++;
             }
             buff.close();
         } catch (IOException e) {
